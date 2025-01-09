6e063c4d702a4a275072ecdb68178042f987417b
Dmytro <Dmytro.Ryshchuk@icloud.com>
2025-01-09 15:47:35 -0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Dmytro Ryshchuk on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
